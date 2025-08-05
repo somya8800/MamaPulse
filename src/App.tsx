2b90272e024b33a4165c1362b001218e28e9f4f6
somya8800 <somyaprabhakar300@gmail.com>
2025-08-05 20:47:16 +0530

    frontend part

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 
 import React from 'react'
 import Home from './Component/home'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App () {
   return (
     <div>
       <Home/>
-      <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
     </div>
     
   )
